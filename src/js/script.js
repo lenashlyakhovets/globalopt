a18310791aa5e90ea13f1cde73bc7f54cacd186c
lena <lena.shlyakhovets@gmail.com>
2021-05-05 12:35:08 +0300

    promo

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+      overlay = document.querySelector('.promo');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+    hamburger.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+    hamburger.classList.remove('active');
+});
+
+window.addEventListener('keydown', (e) => {
+    if (e.key == 'Escape') {
+        menu.classList.remove('active');
+        hamburger.classList.remove('active');
+    }
+});
+
+overlay.addEventListener('click', (e) => {
+    if (e.target == overlay) {
+        menu.classList.remove('active');
+        hamburger.classList.remove('active');
+    }
+});
